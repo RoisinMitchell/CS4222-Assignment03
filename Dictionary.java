9745b36bf1426a9229833d4075f90064f5674a1a
RoisinMitchell <mitchell.roisin@gmail.com>
2023-04-07 16:32:26 +0100

    Optimising Dictionary Method
    Removed function to initialise array with first word (Unnecessary)

--- Dictionary.java
+++ Dictionary.java
@@ -1,54 +1,49 @@
+/*
+ ============================================================================
+ Name        : Dictionary.java
+ Author      : Roisin Mitchell
+ ID          : 21193762
+ Description : A dictionary class with methods to read csv file and add
+               or remove words dependent on parameters passed.
+ ============================================================================
+
+  I am assuming from the brief that I am loading a dictionary
+  that contains words, commas and spaces only, and no special characters or numbers.
+ */
+
+
 import java.io.*; // for File
 import java.util.*; // for Scanner
 
-
-
 public class Dictionary {
-
-
-    ///Constructor Method
     public ArrayList<String> Dictionary(String filepath, int shortest, int longest){
+        //Initialising arraylist that is returned by this method
         ArrayList<String> words = new ArrayList<String>();
-        String wordToken = "";
+
         try {
             Scanner dataSource = new Scanner(new File(filepath));
-            wordToken = dataSource.next();
-            //Initialising array with first word, ignoring words not in specified range
-            boolean result = false;
-            while (result == false) {
-                if (wordToken.length() <= shortest) {
-                    wordToken = dataSource.next();
-                } else if (wordToken.length() > longest) {
-                    wordToken = dataSource.next();
-                } else {
-                    words.add(wordToken.toUpperCase());
-                    result = true;
-                }
-            }
-            //
+            String word = "";
+            String[] parsedWords;
+
             while(dataSource.hasNext()) {
-                wordToken = dataSource.next().toUpperCase();
-                String[] parsedWords = wordToken.split(",");
-
-
+                //Changing each token read in to uppercase
+                word = dataSource.next().toUpperCase();
+                parsedWords = word.split(",");
                 for(int i = 0; i <= parsedWords.length-1; i++) {
-                    int searchIndex = Collections.binarySearch(words, parsedWords[i]);
-                    if (searchIndex < 0) {
-                        searchIndex = ((searchIndex) * -1) - 1;
-                        if (wordToken.length() >= shortest && wordToken.length() <= longest) {
-                            words.add(searchIndex, parsedWords[i].trim());
+                    String parsedWord = parsedWords[i].trim();
+                    int index = Collections.binarySearch(words, parsedWord);
+                    if (index < 0) {
+                        //binarySearch() returns the location as a negative value offset by +1
+                        index = ((index) * -1) - 1;
+                        if (parsedWord.length() >= shortest && parsedWord.length() <= longest) {
+                            //Adding word to arraylist trimmed of spaces
+                                words.add(index, parsedWord);
                         }
-
                     }
                 }
             }
-
         }catch(IOException e) {
-            System.out.println("Error!");
-        }
-
-        if(words.size() == 0){
-            return words;
+            System.out.println("Cannot Read File!");
         }
         return words;
     }
